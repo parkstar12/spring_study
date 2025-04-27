132e51310b482a01e750ed71617c33f71690ec45
parkstar12 <zzanggu12280@naver.com>
2025-04-27 16:16:43 +0900

    .

--- week2/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ week2/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,26 +1,27 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
     //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
-    }
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository=memberRepository;
         this.discountPolicy=discountPolicy;
     }
@@ -32,4 +33,8 @@ public class OrderServiceImpl implements OrderService {
 
         return new Order(memberId,itemName, itemPrice,discountPrice);
     }
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+
 }
